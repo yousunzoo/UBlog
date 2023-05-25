39442af282b714770f1259032d7f41039a07afbd
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-25 14:08:54 +0900

    Design: 현재 위치에 따른 nav li 색상 변경 Header 컴포넌트 생성, usePathname으로 현재 pathname 확인 header padding 값 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link';
 import './globals.css';
 import { Noto_Sans } from 'next/font/google';
-import Image from 'next/image';
+import Header from '@/components/Header';
 const noto = Noto_Sans({ weight: '500', subsets: ['latin'] });
 
 export const metadata = {
@@ -16,31 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<link rel='shortcut icon' href='/icons/favicon.ico' />
 			</head>
 			<body className={`${noto.className} mx-auto flex w-full max-w-screen-2xl flex-col`}>
-				<header className='fixed left-0 top-0 z-10 w-full border-b border-slate-200 bg-white/80 px-6 py-3 dark:border-zinc-700 dark:bg-black/80'>
-					<div className='mx-auto flex max-w-screen-2xl items-center justify-between'>
-						<Link href='/' className='flex items-center gap-1 text-2xl font-bold text-black dark:text-white'>
-							<Image src='/images/logo.png' alt='logo' width={50} height={50} />
-							<h1>{metadata.title}</h1>
-						</Link>
-						<nav>
-							<ul className='flex text-slate-500 dark:text-gray-50'>
-								<li className='mx-2'>
-									<Link href='/'>Home</Link>
-								</li>
-								<li className='mx-2'>
-									<Link href='/about'>About</Link>
-								</li>
-								<li className='mx-2'>
-									<Link href='/posts'>Posts</Link>
-								</li>
-								<li className='mx-2'>
-									<Link href='/contact'>Contact</Link>
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</header>
-				<main className='pt-[65px]'>{children}</main>
+				<Header title={metadata.title} />
+				<main className='pt-[67px]'>{children}</main>
 			</body>
 		</html>
 	);
