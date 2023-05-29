292384b7e29a75a8a2b7b8a98e30e43b1321f194
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-30 01:11:41 +0900

    Design: 포스트 페이지 디자인

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,7 +2,11 @@ import remarkGfm from 'remark-gfm';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 function MarkdownViewer({ content }: { content: string }) {
-	return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+	return (
+		<ReactMarkdown className='prose lg:prose-xl' remarkPlugins={[remarkGfm]}>
+			{content}
+		</ReactMarkdown>
+	);
 }
 
 export default MarkdownViewer;
