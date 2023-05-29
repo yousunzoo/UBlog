a81c24e1bc4e952722316522e15b4b143f77ff71
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-30 00:51:51 +0900

    Design: 카테고리 스타일링

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -8,12 +8,12 @@ type Props = {
 function Categories({ categories, selected, onClick }: Props) {
 	return (
 		<section className='p-4 text-center'>
-			<h2>카테고리</h2>
+			<h2 className='mb-2 border-b border-emerald-500 pb-2 text-lg font-bold'>카테고리</h2>
 			<ul>
 				{categories.map((category) => (
 					<li
 						key={category}
-						className={`${selected === category ? 'font-bold' : ''}`}
+						className={`cursor-pointer hover:text-emerald-500 ${selected === category && 'text-emerald-600'}`}
 						onClick={() => onClick(category)}>
 						{category}
 					</li>
