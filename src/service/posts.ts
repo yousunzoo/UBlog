b413300fa53f43ed53288f4f129324d3811fe566
yousunzoo <dpfvmffjqm@gmail.com>
2023-05-30 00:52:51 +0900

    Feat: Post 컴포넌트 클릭하면 해당 페이지로 이동하는 함수 구현

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,12 +10,7 @@ export type Post = {
 	featured: boolean;
 };
 
-export async function getAllPosts(): Promise<Post[]> {
-	const filePath = path.join(process.cwd(), 'data', 'posts.json');
-	return readFile(filePath, 'utf-8')
-		.then<Post[]>(JSON.parse)
-		.then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
-}
+export type PostData = Post & { content: string };
 
 export async function getFeaturedPosts(): Promise<Post[]> {
 	const posts = await getAllPosts();
@@ -25,4 +20,20 @@ export async function getFeaturedPosts(): Promise<Post[]> {
 export async function getNonFeaturedPosts(): Promise<Post[]> {
 	const posts = await getAllPosts();
 	return posts.filter((post) => !post.featured);
+}
+
+export async function getAllPosts(): Promise<Post[]> {
+	const filePath = path.join(process.cwd(), 'data', 'posts.json');
+	return readFile(filePath, 'utf-8')
+		.then<Post[]>(JSON.parse)
+		.then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+	const filePath = path.join(process.cwd(), 'data/posts', `${fileName}.md`);
+	const metadata = await getAllPosts().then((posts) => posts.find((post) => post.path === fileName));
+	if (!metadata) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+	const content = await readFile(filePath, 'utf-8');
+	return { ...metadata, content };
 }
